44572bb3418ed8842cc2b42b0d8345c150db78e5
imsamad <73896798+imsamad@users.noreply.github.com>
2022-06-23 11:37:47 +0000

    Deploying to gh-pages from @ imsamad/nextjs-github-actions@3c7eed2e83bfc51193ed390428189e0dcc8a639b 🚀

--- _next/static/j8BuoTJdQ___ATwBCT3qp/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-65ce1933d039b5b1.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
