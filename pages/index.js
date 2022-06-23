3c7eed2e83bfc51193ed390428189e0dcc8a639b
Abdus Samad <imsamad00@gmail.com>
2022-06-23 17:06:51 +0530

    added img

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import React, { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import axios from 'axios';
+// import Image from 'next/image';
 const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
 const Index = ({ stories }) => {
   const router = useRouter();
-  console.log('check', router.isFallback);
-  console.log('propds ', stories);
+  // console.log('check', router.isFallback);
+  // console.log('propds ', stories);
   // useEffect(() => {
   //   (async () => {
   //     console.log('fetching');
@@ -16,6 +17,19 @@ const Index = ({ stories }) => {
   // }, []);
   return (
     <div>
+      <div
+        style={{
+          border: '2px solid gray',
+          borderRadius: '0.5rem',
+          height: '320px',
+          margin: 10,
+          width: '420px',
+          display: 'grid',
+          placeItems: 'center',
+        }}
+      >
+        <img height={300} width={400} src="/pix/samad.jpeg" alt="my-photo" />
+      </div>
       {stories.map((story) => (
         <h2 key={story._id} style={{ marginTop: 4, padding: 2 }}>
           {story.title}
